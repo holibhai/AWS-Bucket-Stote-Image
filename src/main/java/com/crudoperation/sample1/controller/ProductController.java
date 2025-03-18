24609b16d9374bef0c9156d9007d3f70655e494b
holibhai <146805057+holibhai@users.noreply.github.com>
2025-03-18 17:57:04 +0530

    product controller added

--- src/main/java/com/crudoperation/sample1/controller/ProductController.java
+++ src/main/java/com/crudoperation/sample1/controller/ProductController.java
@@ -4,7 +4,9 @@ import com.crudoperation.sample1.model.Product;
 import com.crudoperation.sample1.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -15,8 +17,8 @@ public class ProductController {
     private ProductService productService;
 
     @PostMapping("/add")
-    public Product add(@RequestBody Product product) {
-        return productService.add(product);
+    public Product add(@RequestPart Product product,@RequestPart(required = false) MultipartFile file) throws IOException {
+        return productService.add(product,file);
     }
 
     @GetMapping
